1e65a24b1eb7eaf43abe4d85ba6498b3980cf917
marwaalarab <73698404+marwaalarab@users.noreply.github.com>
2022-06-01 20:30:07 +0100

    Add common  covariables
    Define covariables codelists and add codelists

---
+++ analysis/codelists.py
@@ -0,0 +1,52 @@
+from cohortextractor import codelist_from_csv, combine_codelists, codelist
+
+covid_codes = codelist_from_csv(
+    "codelists/user-RochelleKnight-confirmed-hospitalised-covid-19.csv",
+    system="icd10",
+    column="code",
+)
+
+covid_primary_care_positive_test = codelist_from_csv(
+    "codelists/opensafely-covid-identification-in-primary-care-probable-covid-positive-test.csv",
+    system="ctv3",
+    column="CTV3ID",
+)
+
+covid_primary_care_code = codelist_from_csv(
+    "codelists/opensafely-covid-identification-in-primary-care-probable-covid-clinical-code.csv",
+    system="ctv3",
+    column="CTV3ID",
+)
+
+covid_primary_care_sequalae = codelist_from_csv(
+    "codelists/opensafely-covid-identification-in-primary-care-probable-covid-sequelae.csv",
+    system="ctv3",
+    column="CTV3ID",
+)
+
+covid_primary_care_positive_test = codelist_from_csv(
+    "codelists/opensafely-covid-identification-in-primary-care-probable-covid-positive-test.csv",
+    system="ctv3",
+    column="CTV3ID",
+)
+
+covid_primary_care_code = codelist_from_csv(
+    "codelists/opensafely-covid-identification-in-primary-care-probable-covid-clinical-code.csv",
+    system="ctv3",
+    column="CTV3ID",
+)
+
+
+opensafely_ethnicity_codes_6 = codelist_from_csv(
+    "codelists/opensafely-ethnicity.csv",
+    system="ctv3",
+    column="Code",
+    category_column="Grouping_6",
+)
+
+primis_covid19_vacc_update_ethnicity = codelist_from_csv(
+    "codelists/primis-covid19-vacc-uptake-eth2001.csv",
+    system="snomed",
+    column="code",
+    category_column="grouping_6_id",
+)
