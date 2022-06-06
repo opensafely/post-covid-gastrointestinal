0fb217761e9746715fe991d65e5d3286c9e009e0
marwaalarab <73698404+marwaalarab@users.noreply.github.com>
2022-06-06 15:18:41 +0100

    Fix GC comments
    Add codelists csv files
    remove codelists.py

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -24,19 +24,6 @@ covid_primary_care_sequalae = codelist_from_csv(
     column="CTV3ID",
 )
 
-covid_primary_care_positive_test = codelist_from_csv(
-    "codelists/opensafely-covid-identification-in-primary-care-probable-covid-positive-test.csv",
-    system="ctv3",
-    column="CTV3ID",
-)
-
-covid_primary_care_code = codelist_from_csv(
-    "codelists/opensafely-covid-identification-in-primary-care-probable-covid-clinical-code.csv",
-    system="ctv3",
-    column="CTV3ID",
-)
-
-
 opensafely_ethnicity_codes_6 = codelist_from_csv(
     "codelists/opensafely-ethnicity.csv",
     system="ctv3",
@@ -49,4 +36,91 @@ primis_covid19_vacc_update_ethnicity = codelist_from_csv(
     system="snomed",
     column="code",
     category_column="grouping_6_id",
+)
+
+smoking_clear = codelist_from_csv(
+    "codelists/opensafely-smoking-clear.csv",
+    system="ctv3",
+    column="CTV3Code",
+    category_column="Category",
+)
+
+smoking_unclear = codelist_from_csv(
+    "codelists/opensafely-smoking-unclear.csv",
+    system="ctv3",
+    column="CTV3Code",
+    category_column="Category",
+)
+
+bmi_obesity_snomed_clinical = codelist_from_csv(
+    "codelists/user-elsie_horne-bmi_obesity_snomed.csv",
+    system="snomed",
+    column="code",
+)
+
+bmi_obesity_icd10 = codelist_from_csv(
+    "codelists/user-elsie_horne-bmi_obesity_icd10.csv",
+    system="icd10",
+    column="code",
+)
+
+# BMI
+bmi_primis = codelist_from_csv(
+    "codelists/primis-covid19-vacc-uptake-bmi.csv",
+    system="snomed",
+    column="code",
+)
+# Total Cholesterol
+cholesterol_snomed = codelist_from_csv(
+    "codelists/opensafely-cholesterol-tests-numerical-value.csv",
+    system="snomed",
+    column="code",
+)
+
+# HDL Cholesterol
+hdl_cholesterol_snomed = codelist_from_csv(
+    "codelists/bristol-hdl-cholesterol.csv",
+    system="snomed",
+    column="code",
+)
+# Carer codes
+carer_primis = codelist_from_csv(
+    "codelists/primis-covid19-vacc-uptake-carer.csv",
+    system="snomed",
+    column="code",
+)
+
+# No longer a carer codes
+notcarer_primis = codelist_from_csv(
+    "codelists/primis-covid19-vacc-uptake-notcarer.csv",
+    system="snomed",
+    column="code",
+)
+
+# Employed by Care Home codes
+carehome_primis = codelist_from_csv(
+    "codelists/primis-covid19-vacc-uptake-carehome.csv",
+    system="snomed",
+    column="code",
+)
+
+# Employed by nursing home codes
+nursehome_primis = codelist_from_csv(
+    "codelists/primis-covid19-vacc-uptake-nursehome.csv",
+    system="snomed",
+    column="code",
+)
+
+# Employed by domiciliary care provider codes
+domcare_primis = codelist_from_csv(
+    "codelists/primis-covid19-vacc-uptake-domcare.csv",
+    system="snomed",
+    column="code",
+)
+
+# Patients in long-stay nursing and residential care
+longres_primis = codelist_from_csv(
+    "codelists/primis-covid19-vacc-uptake-longres.csv",
+    system="snomed",
+    column="code",
 )
