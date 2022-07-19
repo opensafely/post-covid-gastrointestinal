2b4ae7192d393b68466276a55775be392f407d90
marwaalarab <73698404+marwaalarab@users.noreply.github.com>
2022-07-19 14:18:09 +0100

    minor action update
    name output index dates

--- analysis/create_project_actions.R
+++ analysis/create_project_actions.R
@@ -169,7 +169,7 @@ actions_list <- splice(
     run = "r:latest analysis/prelim.R",
     needs = list("vax_eligibility_inputs","generate_study_population_prelim"),
     highly_sensitive = list(
-      cohort = glue("output/index_dates.csv")
+      index_dates = glue("output/index_dates.csv")
     )
   ),
 
