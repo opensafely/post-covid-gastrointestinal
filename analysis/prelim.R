e8191c7812e683a78ad83a05572d0affc3bf8f6e
Kurt Taylor <kurt.taylor@bristol.ac.uk>
2022-08-09 12:42:06 +0100

    update prelim.R to speed up

--- analysis/prelim.R
+++ analysis/prelim.R
@@ -14,6 +14,9 @@ eligibility_offset <- 84
 #Read in the output of study_definition_prelim and add dates variables
 prelim_data <- arrow::read_feather("output/input_prelim.feather") 
 prelim_data <- prelim_data %>%
+  mutate(delta_date = delta_date,
+         delta_end_date = delta_end_date,
+         all_eligible_date = all_eligible_date) %>%
   mutate(across(c(contains("_date")), 
                 ~ floor_date(
                   as.Date(., format="%Y-%m-%d"),
@@ -22,16 +25,13 @@ prelim_data <- prelim_data %>%
          vax_date_eligible_offset = vax_date_eligible + days(eligibility_offset),
          index_prevax = as.Date(study_dates$pandemic_start)) %>% 
   rowwise() %>%             
-  mutate(index_vax = max(c(vax_date_covid_2_offset,as.Date(rep(delta_date,nrow(prelim_data)))),na.rm=T),
-         index_unvax =  max(c(vax_date_eligible_offset,as.Date(rep(delta_date,nrow(prelim_data)))),na.rm=T),
-         end_vax = min(c(death_date,as.Date(rep(delta_end_date,nrow(prelim_data)))),na.rm=T),
-         end_unvax = min(c(death_date,as.Date(rep(delta_end_date,nrow(prelim_data)))),na.rm=T),
-         end_prevax = min(c(vax_date_eligible,death_date,vax_date_covid_1,as.Date(rep(all_eligible_date))),na.rm=T))
+  mutate(index_vax = max(c(vax_date_covid_2_offset, delta_date), na.rm=T),
+         index_unvax =  max(c(vax_date_eligible_offset, delta_date), na.rm=T),
+         end_vax = min(c(death_date, delta_end_date), na.rm=T),
+         end_unvax = min(c(death_date, delta_end_date), na.rm=T),
+         end_prevax = min(c(vax_date_eligible,death_date, vax_date_covid_1, all_eligible_date), na.rm=T)) %>%
+  dplyr::select(-c(delta_date, delta_end_date, all_eligible_date))
 
 
 #Write data to csv file 
 write_csv(prelim_data, "output/index_dates.csv")
-
-
-
-
