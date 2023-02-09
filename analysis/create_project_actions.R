4ee9c48ecf86662d3f6751bb8607dd670274fa8e
marwaalarab <73698404+marwaalarab@users.noreply.github.com>
2023-02-09 12:19:23 +0000

    remove stage 6 action

--- analysis/create_project_actions.R
+++ analysis/create_project_actions.R
@@ -381,16 +381,16 @@ actions_list <- splice(
     )
     
   ),
-comment("Stage 6 - make model output"),
+# comment("Stage 6 - make model output"),
   
-  action(
-    name = "make_model_output",
-    run = "r:latest analysis/model/make_model_output.R",
-    needs = as.list(paste0("cox_ipw-",success$name)),
-    moderately_sensitive = list(
-      model_output = glue("output/model_output.csv")
-    )
-  ),
+#   action(
+#     name = "make_model_output",
+#     run = "r:latest analysis/model/make_model_output.R",
+#     needs = as.list(paste0("cox_ipw-",success$name)),
+#     moderately_sensitive = list(
+#       model_output = glue("output/model_output.csv")
+#     )
+#   ),
 )
 
   
