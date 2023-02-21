31cc6e0f8f36c03540b3dff5c402c13085662ac7
marwaalarab <73698404+marwaalarab@users.noreply.github.com>
2023-02-21 09:55:49 +0000

    Create utility.R

---
+++ analysis/utility.R
@@ -0,0 +1,6 @@
+# Rounding function for redaction ----------------------------------------------
+
+roundmid_any <- function(x, to=1){
+  # like ceiling_any, but centers on (integer) midpoint of the rounding points
+  ceiling(x/to)*to - (floor(to/2)*(x!=0))
+}
