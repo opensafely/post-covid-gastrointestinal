4cb0c8ce8714f55647dd631a89d65f9a6b916ff6
marwaalarab <73698404+marwaalarab@users.noreply.github.com>
2023-03-14 17:48:09 +0000

    Update prelim.R

--- analysis/prelim.R
+++ analysis/prelim.R
@@ -26,8 +26,7 @@ prelim_data <- prelim_data %>%
          index_unvax =  max(c(vax_date_eligible_offset, delta_date), na.rm=T),
          end_vax = min(c(death_date, delta_end_date), na.rm=T),
          end_unvax = min(c(death_date, delta_end_date), na.rm=T),
-         end_prevax = min(c(vax_date_eligible,death_date, vax_date_covid_1, all_eligible_date), na.rm=T)) 
-
+         end_prevax = min(c(death_date, delta_end_date), na.rm=T)
 
 #Write data to csv file 
 write_csv(prelim_data, "output/index_dates.csv.gz")
