a4756498562d2563230e79a4f9c66b4621e51118
marwaalarab <73698404+marwaalarab@users.noreply.github.com>
2023-05-19 15:17:30 +0100

    update fig1

--- analysis/figures/figure1.R
+++ analysis/figures/figure1.R
@@ -4,16 +4,14 @@ library(tidyverse)
 library(ggplot2)
 
 # Define results directory
-results_dir <- "/Users/cu20932/Library/CloudStorage/OneDrive-SharedLibraries-UniversityofBristol/grp-EHR - OS outputs/models"
+results_dir <- "/Users/cu20932/Library/CloudStorage/OneDrive-SharedLibraries-UniversityofBristol/grp-EHR - OS outputs/Extended followup/models/17-05-2023"
 
 #################
 #1- Get data
 #################
 
 disregard<- str_to_title(c("out_date_bowel_ischaemia", "out_date_intestinal_obstruction", "out_date_nonalcoholic_steatohepatitis", "out_date_variceal_gi_bleeding", "out_date_belching"))
-#symptoms 
-symptoms<- str_to_title(c("ibs", "nausea", "vomiting", "bloody_stools", "abdominal_paindiscomfort", "abdominal_distension", "diarrhoea"))
-estimates <-read.csv(paste0(results_dir, "/model_output_main_17-02-23.csv"))  %>%
+estimates <-read.csv(paste0(results_dir,"/model_output.csv"))  %>%
   # Extract outcomes to plot
   filter(!outcome %in% disregard) %>%
   filter(model=="mdl_max_adj")%>%
@@ -38,34 +36,25 @@ estimates <- estimates %>%
   )
 
 # Specify group colors and line types
-analysis_linetypes <- c("covid_pheno_hospitalised" = "solid", "covid_pheno_non_hospitalised" = "dashed")
 
 # Factor variables for ordering
 estimates <- estimates %>%
   mutate(cohort = factor(cohort, levels = c("prevax", "vax", "unvax")),
-         
-         analysis = factor(analysis, levels = c("main", "covid_pheno_hospitalised", "covid_pheno_non_hospitalised")),
-         linetype = factor(analysis_linetypes[analysis], levels = c("solid", "dashed")))
+                 )
 
 # Rename adjustment groups
 levels(estimates$cohort) <- list("Pre-vaccination (Jan 1 2020 - Dec 14 2021)"="prevax", "Vaccinated (Jun 1 2021 - Dec 14 2021)"="vax","Unvaccinated (Jun 1 2021 - Dec 14 2021)"="unvax")
 
-####################
-#3-Data to plot
-####################
-estimates_symptoms<-estimates %>%
-  filter(outcome%in% symptoms)
 
-estimates_others<-estimates%>%
-  anti_join(estimates_symptoms)
+
 
 ####################
-#3-Plotting functio
+#3-Plotting function
 ####################
 plot_estimates <- function(df,name) {
   pd <- position_dodge(width = 0.5)
   
-  p <- ggplot(df, aes(x = outcome_time_median/7, y = hr, color = colour_cohort, linetype = linetype)) +
+  p <- ggplot(df, aes(x = outcome_time_median/7, y = hr, color = colour_cohort)) +
     geom_line() +
     geom_point(size = 2, position = pd) +
     geom_hline(mapping = aes(yintercept = 1), colour = "#A9A9A9") +
@@ -76,12 +65,14 @@ plot_estimates <- function(df,name) {
                   position = pd) + 
     scale_color_manual(values = levels(df$colour_cohort), labels = levels(df$cohort)) +
     #labs(x = "Time (days)", y = "Hazard ratio", color = "Cohort", linetype = "Analysis")+
-    guides(linetype = "none", color = guide_legend(nrow = 3)) +
+    guides( color = guide_legend(nrow = 3)) +
     guides(fill=ggplot2::guide_legend(ncol = 1, byrow = TRUE) ) +
     facet_wrap(~outcome) +
     theme_minimal() +
     labs(x = "\nWeeks since COVID-19 diagnosis", y = "Hazard ratio and 95% confidence interval") +
     scale_x_continuous(breaks = seq(0, max(df$outcome_time_median)/7, 4)) +  # display labels at 4-week intervals
+    scale_y_continuous(lim = c(0.25,32), breaks = c(0.25,0.5,1,2,4,8,16,32), trans = "log")+ 
+
     theme(panel.grid.major.x = element_blank(),
           panel.grid.minor = element_blank(),
           panel.spacing.x = unit(0.5, "lines"),
@@ -93,12 +84,11 @@ plot_estimates <- function(df,name) {
           plot.margin = margin(1, 1, 1, 1, "cm"),
           text = element_text(size = 12),
     )
-  ggsave(paste0("output/Figure_1_all_cohorts_",name,"_main.png"), height = 297, width = 210, unit = "mm", dpi = 600, scale = 1)
+  # ggsave(paste0("output/Figure_1_all_cohorts_extfup_",name,"_main.png"), height = 297, width = 210, unit = "mm", dpi = 600, scale = 1)
   
-  #return(p)
+  return(p)
 }
 
 
-p_symptoms <- plot_estimates(estimates_symptoms,"Symptoms")
-p_others<- plot_estimates(estimates_others,"Others")
+ plot_estimates(estimates,"Others")
 
