62e81a28180d87a2edc4f257b56a8a7cd293333f
marwaalarab <73698404+marwaalarab@users.noreply.github.com>
2023-06-21 16:12:46 +0100

    clear memory

--- analysis/descriptives/initial_input_counts.R
+++ analysis/descriptives/initial_input_counts.R
@@ -33,13 +33,15 @@ count_list <- lapply(df_prepro_list, count_input)
 count_df <- t(data.frame(do.call(rbind, count_list)))
 colnames(count_df) <- dataset_names
 write.table(count_df, quote = FALSE, row.names = TRUE, col.names = TRUE, "output/not-for-review/study_counts_prepro.txt")
-
+rm(count_df)
+gc()
 # Count non-NA outcome and covars events for raw data
 count_list_sd <- lapply(df_list_sd, count_input)
 count_df_sd <- t(data.frame(do.call(rbind, count_list_sd)))
 colnames(count_df_sd) <- dataset_names
 write.table(count_df_sd, quote = FALSE, row.names = TRUE, col.names = TRUE, "output/not-for-review/study_counts_sd.txt")
-
+rm(count_df_sd)
+gc()
 # # Summary data
 for (i in seq_along(dataset_names)) {
   file_name_prepro <- paste0("output/not-for-review/describe_prepro_", dataset_names[i], ".txt")
@@ -49,8 +51,8 @@ for (i in seq_along(dataset_names)) {
   describe_data(df_list_sd[[i]], file_name_sd)
 }
 
-
-
+rm (df_prepro_list)
+gc()
 
 
 
