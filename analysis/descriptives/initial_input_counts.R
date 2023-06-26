ae3a1121f2703eb06d71caa72575cddd2678e9ef
marwaalarab <73698404+marwaalarab@users.noreply.github.com>
2023-06-26 18:18:32 +0100

    Update initial_input_counts.R
    change format

--- analysis/descriptives/initial_input_counts.R
+++ analysis/descriptives/initial_input_counts.R
@@ -14,10 +14,13 @@ if(length(args)==0){
 count_input <- function(df) {
   out_df <- df %>% 
     select(matches("^(out_date)"))
-  sapply(out_df, function(x) {
+  counts <- sapply(out_df, function(x) {
     x <- ifelse(is.na(x), "", as.character(x))
     sum(x != "", na.rm = TRUE)
   })
+    count_df <- data.frame(outcome = names(counts), count = counts, row.names = NULL)
+
+  
 }
 
 
@@ -42,22 +45,23 @@ message(paste0("After preprocessing:\n",str(df_prepro_list)))
 
 # Count non-NA outcome and covars events for preprocessed data
 count_list <- count_input(df_prepro_list)
-count_df <- t(count_list)
+# count_df <- t(count_list)
 counts_prepro_file<- paste0("output/not-for-review/study_counts_prepro_",cohort_name,".txt")
-write.table(count_df, quote = FALSE, row.names = TRUE, col.names = TRUE, counts_prepro_file)
-rm(count_df)
+write.table(count_list, quote = FALSE, row.names = FALSE, col.names = TRUE, file = counts_prepro_file)
+rm(count_list)
 gc()
+
 # Count non-NA outcome and covars events for raw data
 count_list_sd <- count_input(df_list_sd)
-count_df_sd <- t( count_list_sd)
-write.table(count_df_sd, quote = FALSE, row.names = TRUE, col.names = TRUE, paste0("output/not-for-review/study_counts_sd_",cohort_name,".txt"))
-rm(count_df_sd)
+write.table(count_list_sd, quote = FALSE, row.names = FALSE, col.names = TRUE, file=paste0("output/not-for-review/study_counts_sd_",cohort_name,".txt"))
+rm(count_list_sd)
 gc()
 # # Summary data
 for (i in seq_along(dataset_names)) {
   file_name_prepro <- paste0("output/not-for-review/describe_prepro_", dataset_names[i], ".txt")
   describe_data(df_prepro_list[[i]], file_name_prepro)
-  
+  rm (df_prepro_list[[i]])
+  gc()
   file_name_sd <- paste0("output/not-for-review/describe_sd_", dataset_names[i], ".txt")
   describe_data(df_list_sd[[i]], file_name_sd)
 }
