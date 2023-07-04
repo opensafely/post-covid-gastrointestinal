0457ddcbf89a591e7f908b03659bb2b6220b758d
marwaalarab <73698404+marwaalarab@users.noreply.github.com>
2023-07-04 20:55:29 +0100

    test type of vars before and after preprocess

--- analysis/preprocess/preprocess_data.R
+++ analysis/preprocess/preprocess_data.R
@@ -39,6 +39,7 @@ message(paste0("After adding death N = ", nrow(df), " rows"))
 print("nchar of vgib date before change of format: ")
 print(table(nchar(as.character(df$out_date_variceal_gi_bleeding)), useNA = "always"))
 df$out_date_variceal_gi_bleeding <- as.Date(df$out_date_variceal_gi_bleeding, format = ifelse(nchar(df$out_date_variceal_gi_bleeding) > 10, "%Y-%m-%d %H:%M:%S", "%Y-%m-%d"))
+
 print("nchar of vgib date after change of format: ")
 print(table(nchar(as.character(df$out_date_variceal_gi_bleeding)), useNA = "always"))
 
