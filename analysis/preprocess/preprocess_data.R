fff7347dfa23ffd265833392c9d18588b93a7ce8
marwaalarab <73698404+marwaalarab@users.noreply.github.com>
2023-07-07 00:50:18 +0100

    uncomment preprocessing code

--- analysis/preprocess/preprocess_data.R
+++ analysis/preprocess/preprocess_data.R
@@ -1,10 +1,11 @@
-# Load libraries ---------------------------------------------------------------
 tictoc::tic()
+# Load libraries ---------------------------------------------------------------
 library(magrittr)
 library(dplyr)
 library(tidyverse)
 library(lubridate)
 library(data.table)
+library(readr)
 
 # Specify command arguments ----------------------------------------------------
 args <- commandArgs(trailingOnly=TRUE)
@@ -19,28 +20,19 @@ if(length(args)==0){
 fs::dir_create(here::here("output", "not-for-review"))
 fs::dir_create(here::here("output", "review"))
 
+input_path<-paste0("output/input_",cohort_name,".csv.gz")
 
+# Get colnames 
+col_names <- fread(input_path, header = TRUE, sep = ",", nrows = 1, stringsAsFactors = FALSE)
 
-
-file_path<-paste0("output/input_",cohort_name,".csv.gz")
-library(readr)
-
-
-
-col_names <- fread(file_path, header = TRUE, sep = ",", nrows = 1, stringsAsFactors = FALSE)
-
-# Filter column names to include only those with "out_date"
+#Get column with "_date"
 out_date_cols <- grep("_date", colnames(col_names), value = TRUE)
+# Set class to date
 col_classes <- setNames(rep("Date", length(out_date_cols)), out_date_cols)
-df <- fread(file_path, colClasses = col_classes)
-
-# Read cohort dataset ---------------------------------------------------------- 
-
-str(df)
-
+df <- fread(input_path, colClasses = col_classes)
 print(paste0("Dataset has been read successfully with N = ", nrow(df), " rows"))
-# Describe data ----------------------------------------------------------------
 
+# Describe data ----------------------------------------------------------------
 sink(paste0("output/not-for-review/describe_",cohort_name,".txt"))
 print(Hmisc::describe(df%>%select("out_date_ibs","out_date_variceal_gi_bleeding","out_date_bowel_ischaemia")))
 print(str(df))
@@ -48,127 +40,106 @@ sink()
 
 message ("Cohort ",cohort_name, " description written successfully!")
 
-# #Add death_date from prelim data
-# prelim_data <- read_csv("output/index_dates.csv.gz") %>%
-#   select(c(patient_id,death_date))
-# df <- df %>% inner_join(prelim_data,by="patient_id")
+#Add death_date from prelim data
+prelim_data <- read_csv("output/index_dates.csv.gz") %>%
+  select(c(patient_id,death_date))
+df <- df %>% inner_join(prelim_data,by="patient_id")
 
-# message("Death date added!")
-# message(paste0("After adding death N = ", nrow(df), " rows"))
-
-
-# # TEST 
-# # print("nchar of vgib date before change of format: ")
-# # print(table(nchar(as.character(df$out_date_variceal_gi_bleeding)), useNA = "always"))
-# # df$out_date_variceal_gi_bleeding <- as.Date(df$out_date_variceal_gi_bleeding, format = ifelse(nchar(df$out_date_variceal_gi_bleeding) > 10, "%Y-%m-%d %H:%M:%S", "%Y-%m-%d"))
-
-# # print("nchar of vgib date after change of format: ")
-# # print(table(nchar(as.character(df$out_date_variceal_gi_bleeding)), useNA = "always"))
-
-
+message("Death date added!")
+message(paste0("After adding death N = ", nrow(df), " rows"))
 
 # # Format columns ---------------------------------------------------------------
-# # dates, numerics, factors, logicals
+#  numerics, factors, logicals
 
-# # df <- df %>%
-# #   # mutate(across(contains("_date"), as.POSIXct),
-# #   # mutate(across(contains("_date"), ~ as.Date(as.character(as.POSIXct(.)), format = "%Y-%m-%d")),
+df <- df %>%
+          mutate( across(contains('_birth_year'),
+                ~ format(as.Date(.,origin='1970-01-01'), "%Y")),
+         across(contains('_num') & !contains('date'), ~ as.numeric(.)),
+         across(contains('_cat'), ~ as.factor(.)),
+         across(contains('_bin'), ~ as.logical(.)))
 
-# #   # mutate(across(c(contains("_date")),
-# #   #               ~ floor_date(as.Date(., format="%Y-%m-%d",origin='1970-01-01'), unit = "days")),
-# #         mutate( across(contains('_birth_year'),
-# #                 ~ format(as.Date(.,origin='1970-01-01'), "%Y")),
-# #          across(contains('_num') & !contains('date'), ~ as.numeric(.)),
-# #          across(contains('_cat'), ~ as.factor(.)),
-# #          across(contains('_bin'), ~ as.logical(.)))
+# Overwrite vaccination information for dummy data and vax cohort only --
 
-
-
-# # Overwrite vaccination information for dummy data and vax cohort only --
-
-# if(Sys.getenv("OPENSAFELY_BACKEND") %in% c("", "expectations") &&
-#    cohort_name %in% c("vax")) {
-#   source("analysis/preprocess/modify_dummy_vax_data.R")
-#   message("Vaccine information overwritten successfully")
-# }
+if(Sys.getenv("OPENSAFELY_BACKEND") %in% c("", "expectations") &&
+   cohort_name %in% c("vax")) {
+  source("analysis/preprocess/modify_dummy_vax_data.R")
+  message("Vaccine information overwritten successfully")
+}
 
 
+#Combine BMI variables to create one history of obesity variable ---------------
+
+df$cov_bin_obesity <- ifelse(df$cov_bin_obesity == TRUE | 
+                               df$cov_cat_bmi_groups=="Obese",TRUE,FALSE)
+#df[,c("cov_num_bmi")] <- NULL
+
+# QC for consultation variable--------------------------------------------------
+#max to 365 (average of one per day)
+df <- df %>%
+  mutate(cov_num_consulation_rate = replace(cov_num_consulation_rate, 
+                                            cov_num_consulation_rate > 365, 365))
 
 
-# #Combine BMI variables to create one history of obesity variable ---------------
+#COVID19 severity --------------------------------------------------------------
 
-# df$cov_bin_obesity <- ifelse(df$cov_bin_obesity == TRUE | 
-#                                df$cov_cat_bmi_groups=="Obese",TRUE,FALSE)
-# #df[,c("cov_num_bmi")] <- NULL
+df <- df %>%
+  mutate(sub_cat_covid19_hospital = 
+           ifelse(!is.na(exp_date_covid19_confirmed) &
+                    !is.na(sub_date_covid19_hospital) &
+                    sub_date_covid19_hospital - exp_date_covid19_confirmed >= 0 &
+                    sub_date_covid19_hospital - exp_date_covid19_confirmed < 29, "hospitalised",
+                  ifelse(!is.na(exp_date_covid19_confirmed), "non_hospitalised", 
+                         ifelse(is.na(exp_date_covid19_confirmed), "no_infection", NA)))) %>%
+  mutate(across(sub_cat_covid19_hospital, factor))
+df <- df[!is.na(df$patient_id),]
+df[,c("sub_date_covid19_hospital")] <- NULL
 
-# # QC for consultation variable--------------------------------------------------
-# #max to 365 (average of one per day)
-# df <- df %>%
-#   mutate(cov_num_consulation_rate = replace(cov_num_consulation_rate, 
-#                                             cov_num_consulation_rate > 365, 365))
-
-
-# #COVID19 severity --------------------------------------------------------------
-
-# df <- df %>%
-#   mutate(sub_cat_covid19_hospital = 
-#            ifelse(!is.na(exp_date_covid19_confirmed) &
-#                     !is.na(sub_date_covid19_hospital) &
-#                     sub_date_covid19_hospital - exp_date_covid19_confirmed >= 0 &
-#                     sub_date_covid19_hospital - exp_date_covid19_confirmed < 29, "hospitalised",
-#                   ifelse(!is.na(exp_date_covid19_confirmed), "non_hospitalised", 
-#                          ifelse(is.na(exp_date_covid19_confirmed), "no_infection", NA)))) %>%
-#   mutate(across(sub_cat_covid19_hospital, factor))
-# df <- df[!is.na(df$patient_id),]
-# df[,c("sub_date_covid19_hospital")] <- NULL
-
-# message("COVID19 severity determined successfully")
+message("COVID19 severity determined successfully")
 
 
-# # Restrict columns and save analysis dataset ---------------------------------
 
-# df1 <- df%>% select(patient_id,"death_date",starts_with("index_date"),
-#                     has_follow_up_previous_6months,
-#                     dereg_date,
-#                      starts_with("end_date_"),
-#                      contains("sub_"), # Subgroups
-#                      contains("exp_"), # Exposures
-#                      contains("out_"), # Outcomes
-#                      contains("cov_"), # Covariates
-#                      contains("qa_"), #quality assurance
-#                      contains("step"), # diabetes steps
-#                      contains("vax_date_eligible"), # Vaccination eligibility
-#                      contains("vax_date_"), # Vaccination dates and vax type 
-#                      contains("vax_cat_")# Vaccination products
-# )
+# Restrict columns and save analysis dataset ---------------------------------
+
+df1 <- df%>% select(patient_id,"death_date",starts_with("index_date"),
+                    has_follow_up_previous_6months,
+                    dereg_date,
+                     starts_with("end_date_"),
+                     contains("sub_"), # Subgroups
+                     contains("exp_"), # Exposures
+                     contains("out_"), # Outcomes
+                     contains("cov_"), # Covariates
+                     contains("qa_"), #quality assurance
+                     contains("vax_date_eligible"), # Vaccination eligibility
+                     contains("vax_date_"), # Vaccination dates and vax type 
+                     contains("vax_cat_")# Vaccination products
+                    )%>% 
+                   select(-matches("tmp_"))
+
+# Restrict columns and save Venn diagram input dataset -----------------------
+
+df2 <- df %>% select(starts_with(c("patient_id","tmp_out_date","out_date")))
+rm(df)
+gc()
 
 
-# df1[,colnames(df)[grepl("tmp_",colnames(df1))]] <- NULL
-# rm(df)
-# gc()
-# # Repo specific preprocessing 
+saveRDS(df1, file = paste0("output/input_",cohort_name,".rds"), compress = "gzip")
 
-# saveRDS(df1, file = paste0("output/input_",cohort_name,".rds"), compress = "gzip")
+message(paste0("Input data saved successfully with N = ", nrow(df1), " rows"))
 
-# message(paste0("Input data saved successfully with N = ", nrow(df1), " rows"))
+# Describe data --------------------------------------------------------------
 
-# # Describe data --------------------------------------------------------------
+sink(paste0("output/not-for-review/describe_input_",cohort_name,"_stage0.txt"))
+print(Hmisc::describe(df1))
+sink()
+rm(df1)
+gc()
+# Describe Venn --------------------------------------------------------------
 
-# # sink(paste0("output/not-for-review/describe_input_",cohort_name,"_stage0.txt"))
-# # print(Hmisc::describe(df1))
-# # sink()
+sink(paste0("output/not-for-review/describe_venn_",cohort_name,".txt"))
+print(Hmisc::describe(df2))
+sink()
 
-# # Restrict columns and save Venn diagram input dataset -----------------------
+saveRDS(df2, file = paste0("output/venn_",cohort_name,".rds"))
 
-# df2 <- df1 %>% select(starts_with(c("patient_id","tmp_out_date","out_date")))
-# str(df2)
-# # Describe data --------------------------------------------------------------
-
-# sink(paste0("output/not-for-review/describe_venn_",cohort_name,".txt"))
-# print(Hmisc::describe(df2))
-# sink()
-
-# saveRDS(df1, file = paste0("output/venn_",cohort_name,".rds"))
-
-# message("Venn diagram data saved successfully")
-# tictoc::toc()
+message("Venn diagram data saved successfully")
+tictoc::toc()
