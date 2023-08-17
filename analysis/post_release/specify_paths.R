d4fc500ebcdfeb4069710fd64f1b852eb5f50daf
marwaalarab <73698404+marwaalarab@users.noreply.github.com>
2023-08-17 12:01:16 +0300

    Create specify_paths.R

---
+++ analysis/post_release/specify_paths.R
@@ -0,0 +1,5 @@
+# This file is used to specify paths. It is in the .gitignore to keep your information secret.
+# To use, please remove "_example" from the file name and add your specific file paths below.
+
+release <- "output/results/" # directory containing all results
+release2 <- "" # directory containing extended table 1
