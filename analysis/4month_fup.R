cd2737174047277eb70f00a2cc950664205d2a58
marwaalarab <73698404+marwaalarab@users.noreply.github.com>
2023-09-27 12:19:24 +0100

    combine all input

--- analysis/4month_fup.R
+++ analysis/4month_fup.R
@@ -2,14 +2,36 @@
 library(dplyr)
 library(readr)
 
+
+# Specify command arguments ----------------------------------------------------
+print('Specify command arguments')
+
+args <- commandArgs(trailingOnly=TRUE)
+
+if(length(args)==0){
+  cohort <- "prevax"
+} else {
+  cohort <- args[[1]]
+}
+
+# Read and filter active analyses
+print('Read and filter active analyses')
 active_analyses <- readr::read_rds("lib/active_analyses.rds")
 active_analyses <- active_analyses%>%
- filter(analysis=="sub_covid_hospitalised" &  grepl("bleeding", outcome, ignore.case = TRUE) )
+filter(analysis=="sub_covid_hospitalised" &  grepl("bleeding", outcome, ignore.case = TRUE) )
 
 
-get_4mo_followup <- function(name) {
-    input <- readRDS(paste0("output/model_input-", name, ".rds"))
+
+# Function to read an combine model inputs
+get_4mo_followup <- function(c) {
+
+    active_analyses <- active_analyses%>%
+                                     filter(cohort==c)
+    cohort_data <- data.frame()
     
+    for (i in 1:length(active_analyses)) {
+    input <- readRDS(paste0("output/model_input-",name,".rds"))
+    input$outcome<-active_analyses[i,"outcome"]
     if ("prevax" %in% name) {
         study_start <- as.Date("2020-01-01", origin = '1970-01-01')
     } else {
@@ -23,13 +45,17 @@ get_4mo_followup <- function(name) {
     
     input$fup_total <- as.numeric(input$fup_stop - input$fup_start)
     
-    input <- input %>% 
+     input <- input %>% 
         filter(fup_total >= 120)
     
-    write_rds("output/fup4mo-",name,".rds")
+    cohort_data <- bind_rows(input)
 }
+print("Writing combined input to file")
+write_rds(cohort_data,paste0("output/model_input_fup4mo_",c,".rds"))
+    }
 
-results <- lapply(active_analyses$name, get_4mo_followup)
+get_4mo_followup("prevax")
+
 
 
 
