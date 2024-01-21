011756ea81d68d06160f77e3cea5e0103ded40d5
marwaalarab <73698404+marwaalarab@users.noreply.github.com>
2024-01-21 03:01:59 +0000

    update active analyses failed

--- analysis/stata/active_analyses_failed.R
+++ analysis/stata/active_analyses_failed.R
@@ -3,14 +3,14 @@
 
 library(dplyr)
 
-model_output_file <- "/Users/cu20932/Library/CloudStorage/OneDrive-SharedLibraries-UniversityofBristol/grp-EHR - OS outputs/Extended followup/models/model_output.csv"
+model_output_file <- "/Users/cu20932/Library/CloudStorage/OneDrive-SharedLibraries-UniversityofBristol/grp-EHR - OS outputs/Day0/models_30_11_2023/model_output_midpoint6.csv"
 active_analyses <- readRDS("lib/active_analyses.rds")
 all_models <- read.csv(model_output_file)
 
-# failed models are those with hr>100 or hr is na
+# failed models are those with hr>100 for days1_28  or hr is na or conf_high is infinity
 
 failed_models <- all_models %>% 
-                            filter(as.numeric(hr)>100 | is.na(hr)) %>% 
+                            filter(as.numeric(hr)>100,term=="days1_28" | is.na(hr) | conf_high=="Inf") %>% 
                             filter(grepl("days\\d+", term))
 
 failed_models_reduced <- failed_models %>% 
