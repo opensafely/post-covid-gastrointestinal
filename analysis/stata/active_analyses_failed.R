2dc76aac7eb9ad4666ff1bc99646ed9f102ac705
marwaalarab <73698404+marwaalarab@users.noreply.github.com>
2024-01-22 01:19:04 +0000

    Update active_analyses_failed.R

--- analysis/stata/active_analyses_failed.R
+++ analysis/stata/active_analyses_failed.R
@@ -14,11 +14,31 @@ failed_models <- all_models %>%
                             filter(grepl("days\\d+", term))
 
 failed_models_reduced <- failed_models %>% 
-                           select( name,analysis)
+                           select( name,analysis,model)
 
  active_analyses_failed<- active_analyses %>% 
                             inner_join(failed_models_reduced,by=c("name","analysis"))
  active_analyses_failed <- active_analyses_failed[!duplicated(active_analyses_failed$name), ]
 
-
-  saveRDS(active_analyses_failed, file = "lib/active_analyses_failed.rds", compress = "gzip")
+# Add models which didn't run at all 
+ success <- readxl::read_excel("../post-covid-outcome-tracker.xlsx",
+                               sheet = "gastrointestinal",
+                               col_types = c("text","text", "text", "text", "text", "text",
+                                             "text", "text", "text", "text", "text",
+                                             "text", "text", 
+                                             "text", "text", "text", "text","text","text","text","text",
+                                             "skip", "skip"))
+ 
+ success <- tidyr::pivot_longer(success,
+                                cols = setdiff(colnames(success),c("outcome","cohort")),
+                                names_to = "analysis") 
+ not_ran<- success %>% filter(value=="NA")%>%
+   filter(!(cohort=="prevax" & analysis=="sub_covid_history"))
+not_ran$name <- paste0("cohort_",not_ran$cohort, "-",not_ran$analysis, "-",not_ran$outcome)
+active_analyses_not_ran<- active_analyses %>% 
+  inner_join(not_ran%>%select(name),by=c("name"))
+# Combine not_ran and failed 
+active_analyses_not_ran$model<-NA
+active_analyses_failed<-rbind(active_analyses_failed,active_analyses_not_ran)
+ 
+saveRDS(active_analyses_failed, file = "lib/active_analyses_failed.rds", compress = "gzip")
