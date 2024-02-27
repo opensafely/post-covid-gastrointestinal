b3309eeb7c5ea7ade7b5760976804e9705a9ac74
marwaalarab <73698404+marwaalarab@users.noreply.github.com>
2024-02-27 13:41:51 +0000

    Update test_dereg_date.R

--- analysis/preprocess/test_dereg_date.R
+++ analysis/preprocess/test_dereg_date.R
@@ -4,12 +4,12 @@ library(readr)
 
 # Load cohort data -------------------------------------------------------------
 print('Load cohort data')
-input <- read_rds(file.path("output", paste0("input_unvax.rds")))%>%select(c(index_date,deregistration_date))
+input <- read_rds(file.path("output", paste0("input_unvax.rds")))%>%select(c(index_date_cohort,deregistration_date))
 
 sink("output/not-for-review/dereg_date_test.txt")
 print (paste0("type of deregistration date: ",(typeof(input$deregistration_date))) )
-print(paste0("class of index date: ",(class(input$index_date))) )
+print(paste0("class of index date: ",(class(input$index_date_cohort))) )
 print(paste0("class of daregistration date: ",(class(input$deregistration_date))))
-print(summary(input%>%select(c(deregistration_date,index_date))))
-print(str(input%>%select(c(deregistration_date,index_date))))
+print(summary(input%>%select(c(deregistration_date,index_date_cohort))))
+print(str(input%>%select(c(deregistration_date,index_date_cohort))))
 sink()
