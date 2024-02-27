3181773db028a7ce60ce9c1f43fc15959aac741a
marwaalarab <73698404+marwaalarab@users.noreply.github.com>
2024-02-27 13:16:15 +0000

    Update test_dereg_date.R

--- analysis/preprocess/test_dereg_date.R
+++ analysis/preprocess/test_dereg_date.R
@@ -8,7 +8,7 @@ input <- read_rds(file.path("output", paste0("input_unvax.rds")))%>%select(c(ind
 
 sink("output/not-for-review/dereg_date_test.txt")
 print (paste0("type of deregistration date: ",(typeof(input$deregistration_date))) )
-print(paste0("class of daeath date: ",(class(input$index_date))) )
+print(paste0("class of index date: ",(class(input$index_date))) )
 print(paste0("class of daregistration date: ",(class(input$deregistration_date))))
 print(summary(input%>%select(c(deregistration_date,index_date))))
 print(str(input%>%select(c(deregistration_date,index_date))))
