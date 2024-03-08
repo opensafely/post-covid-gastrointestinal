4899ca3f8fa77b8a78bd001c5a3b884bf08cbc6c
marwaalarab <73698404+marwaalarab@users.noreply.github.com>
2024-03-08 23:57:35 +0000

    Update active_analyses_failed.R
    update to comply with success read from actions on the server

--- analysis/stata/active_analyses_failed.R
+++ analysis/stata/active_analyses_failed.R
@@ -3,7 +3,7 @@
 
 library(dplyr)
 
-model_output_file <- "/Users/cu20932/Library/CloudStorage/OneDrive-SharedLibraries-UniversityofBristol/grp-EHR - OS outputs/Day0/models_30_11_2023/model_output_midpoint6.csv"
+model_output_file <- "/Users/cu20932/Library/CloudStorage/OneDrive-SharedLibraries-UniversityofBristol/grp-EHR - OS outputs/death_fix20240305/model_output_midpoint6.csv"
 active_analyses <- readRDS("lib/active_analyses.rds")
 all_models <- read.csv(model_output_file)
 
@@ -21,23 +21,15 @@ failed_models_reduced <- failed_models %>%
  active_analyses_failed <- active_analyses_failed[!duplicated(active_analyses_failed$name), ]
 
 # Add models which didn't run at all 
- success <- readxl::read_excel("../post-covid-outcome-tracker.xlsx",
-                               sheet = "gastrointestinal",
-                               col_types = c("text","text", "text", "text", "text", "text",
-                                             "text", "text", "text", "text", "text",
-                                             "text", "text", 
-                                             "text", "text", "text", "text","text","text","text","text",
-                                             "skip", "skip"))
+ actions <- read.csv("lib/actions_20240305.csv")
  
- success <- tidyr::pivot_longer(success,
-                                cols = setdiff(colnames(success),c("outcome","cohort")),
-                                names_to = "analysis") 
- not_ran<- success %>% filter(value=="NA")%>%
-   filter(!(cohort=="prevax" & analysis=="sub_covid_history"))
-not_ran$name <- paste0("cohort_",not_ran$cohort, "-",not_ran$analysis, "-",not_ran$outcome)
+
+ not_ran_actions<- actions %>% filter(!success)
+ not_ran_actions <- not_ran_actions %>%
+  mutate(name= gsub("cox_ipw-","",model))
 active_analyses_not_ran<- active_analyses %>% 
-  inner_join(not_ran%>%select(name),by=c("name"))
-# Combine not_ran and failed 
+  inner_join(not_ran_actions%>%select(name),by=c("name"))
+# Combine not_ran_actions and failed 
 active_analyses_not_ran$model<-NA
 active_analyses_failed<-rbind(active_analyses_failed,active_analyses_not_ran)
  
