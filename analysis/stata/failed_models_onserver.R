fba958a08d00e076968930b159998a012b683673
marwaalarab <73698404+marwaalarab@users.noreply.github.com>
2024-03-18 14:11:57 +0000

    filename

--- analysis/stata/failed_models_onserver.R
+++ analysis/stata/failed_models_onserver.R
@@ -6,7 +6,7 @@ failed_models <- model_output %>%
                             filter(as.numeric(hr)>100,term=="days1_28"| term=="days0_1", as.numeric(hr>1000) |grepl("^days[0-9]", term), is.na(hr) | conf_high=="Inf") %>% 
                             filter(grepl("days\\d+", term))
 unique_models<- unique(failed_models$name)
-write.csv(failed_models,"output/not-for-review/failed_models_onserver.csv",row.names=FALSE)
-sink("output/not-for-review/unique_failed_models.txt")
+write.csv(failed_models,"output/failed_models_onserver.csv",row.names=FALSE)
+sink("output/unique_failed_models.txt")
 print(unique_models)
 sink()
