305f3de90efe97ac4ca958afebe6d7661aac101a
marwaalarab <73698404+marwaalarab@users.noreply.github.com>
2024-03-19 23:57:18 +0000

    tidy figure 2 code

--- analysis/figures/figure2.R
+++ analysis/figures/figure2.R
@@ -4,19 +4,14 @@ library(tidyverse)
 library(ggplot2)
 library(stringr)
 # Define results directory
-# results_dir <- "/Users/cu20932/Library/CloudStorage/OneDrive-SharedLibraries-UniversityofBristol/grp-EHR - OS outputs/Extended followup/models/"
-results_dir <-"/Users/cu20932/Library/CloudStorage/OneDrive-SharedLibraries-UniversityofBristol/grp-EHR - OS outputs/Day0/models_30_11_2023/"
-# output_dir <-"/Users/cu20932/Library/CloudStorage/OneDrive-SharedLibraries-UniversityofBristol/grp-EHR - OS outputs/Extended followup/figures/"
-output_dir <- "/Users/cu20932/Library/CloudStorage/OneDrive-SharedLibraries-UniversityofBristol/grp-EHR - OS outputs/Day0/figures/"
+df <- readr::read_csv("output/plot_model_output.csv",
+                      show_col_types = FALSE) 
 #################
 #1- Get data
 #################
 
-# disregard<- str_to_title(c("out_date_bowel_ischaemia", "out_date_intestinal_obstruction"))
-estimates_file<- paste0(results_dir,"model_output_midpoint6.csv")
-estimates <-read_csv(estimates_file)%>%
-  # Extract outcomes to plot
-  # filter(!outcome %in% disregard) %>%
+df<-df%>%
+  
   filter(model=="mdl_max_adj")%>%
   #keep only rows with time points 
   filter(grepl("days\\d+", term))%>%
@@ -25,17 +20,20 @@ estimates <-read_csv(estimates_file)%>%
   # Modify outcome names
   mutate(outcome = str_remove(outcome, "out_date_")) %>%
   mutate(outcome = str_to_title(outcome)) %>%
-  filter(!is.na(hr) & hr != "" & hr!="[redact]")
+  filter(!is.na(hr) & hr != "" & hr!="[redact]")%>% 
+  filter(!outcome %in%c("Acute_pancreatitis","Peptic_ulcer","Nonvariceal_gi_bleeding","Appendicitis")) %>% 
+  filter(analysis=="main")
+ 
 
 # Set numeric cols to numeric
-numeric_cols <- c("lnhr", "se_lnhr", "hr", "conf_low", "conf_high", "N_total_midpoint6", "N_exposed_midpoint6", "N_events_midpoint6", "person_time_total", "outcome_time_median")
-estimates[numeric_cols] <- lapply(estimates[numeric_cols], as.numeric)
+numeric_cols <- c( "hr", "conf_low", "conf_high", "outcome_time_median")
+df[numeric_cols] <- lapply(df[numeric_cols], as.numeric)
 
 
 ##################
 #2-Format
 #################
-estimates <- estimates %>% 
+df <- df %>% 
   mutate(colour_cohort = case_when(
     cohort == "prevax" ~ "#d2ac47",
     cohort == "vax" ~ "#58764c",
@@ -46,28 +44,26 @@ estimates <- estimates %>%
   )
 
 # Factor variables for ordering
-estimates <- estimates %>%
+df <- df %>%
   mutate(cohort = factor(cohort, levels = c("prevax", "vax", "unvax")),
                  )
 
 # Rename adjustment groups
-levels(estimates$cohort) <- list("Pre-vaccination (Jan 1 2020 - Dec 14 2021)"="prevax", "Vaccinated (Jun 1 2021 - Dec 14 2021)"="vax","Unvaccinated (Jun 1 2021 - Dec 14 2021)"="unvax")
-
-estimates$outcome_label<- str_replace_all(estimates$outcome,"_"," ")
-estimates$outcome_label<- str_replace(estimates$outcome_label,"disease","")%>%str_trim()
-estimates$outcome_label<- str_replace(estimates$outcome_label,"gi","gastrointestinal")
+levels(df$cohort) <- list("Pre-vaccination (Jan 1 2020 - Dec 14 2021)"="prevax", "Vaccinated (Jun 1 2021 - Dec 14 2021)"="vax","Unvaccinated (Jun 1 2021 - Dec 14 2021)"="unvax")
+df<- df %>% 
+mutate(outcome = ifelse(outcome=="Ibs","Irritable_bowel_syndrom",outcome))
+df$outcome_label<- str_replace_all(df$outcome,"_"," ")
+df$outcome_label<- str_replace(df$outcome_label,"disease","")%>%str_trim()
+df$outcome_label<- str_replace(df$outcome_label,"gi","gastrointestinal")
 # labels 
-
+outcomes_order <- c( "Lower gastrointestinal bleeding", "Upper gastrointestinal bleeding","Gastro oesophageal reflux",
+                      "Gallstones","Irritable bowel syndrom","Dyspepsia","Nonalcoholic steatohepatitis","Variceal gastrointestinal bleeding") 
+df$outcome_label <- factor(df$outcome_label, levels = outcomes_order)
 ####################
 #3-Plotting function
 ####################
 plot_estimates <- function(df) {
   pd <- position_dodge(width = 0.25)
-  
-  outcomes_order <- c("Nonvariceal gastrointestinal bleeding", "Lower gastrointestinal bleeding", "Upper gastrointestinal bleeding","Gastro oesophageal reflux",
-                      "Gallstones","Ibs","Acute pancreatitis","Peptic ulcer","Appendicitis","Nonalcoholic steatohepatitis") 
-  df$outcome_label <- factor(df$outcome_label, levels = outcomes_order)
- 
    p <- ggplot(df, aes(x = outcome_time_median/7, y = hr, color = colour_cohort)) +
     geom_line() +
     geom_point(size = 2, position = pd) +
@@ -75,7 +71,7 @@ plot_estimates <- function(df) {
      geom_errorbar(size = 1.2, 
                    aes(ymin = ifelse(conf_low < 0.25, 0.25, conf_low),
                        ymax = ifelse(conf_high > 64, 64, conf_high),
-                       width = 0.25,linetype = "dashed"), 
+                       width = 0.25), 
                   position = pd) +
     scale_color_manual(values = levels(df$colour_cohort), labels = levels(df$cohort)) +
     guides( color = guide_legend(nrow = 3)) +
@@ -98,14 +94,14 @@ plot_estimates <- function(df) {
           text = element_text(size = 12),
           strip.text= element_text(size=12, face="bold")
     )
-   ggsave(paste0(output_dir,"Figure_2.png"), height = 297, width = 210, unit = "mm", dpi = 600, scale = 1)
+   ggsave(paste0("output/post_release/Figure_2.png"), height = 297, width = 210, unit = "mm", dpi = 600, scale = 1)
   
   return(p)
 }
 
-estimates_main <-estimates[estimates$analysis=="main" ,] %>%
-  filter(!outcome %in%c("Acute_pancreatitis","Peptic_ulcer","Nonvariceal_gi_bleeding","Appendicitis"))
- 
-p<-plot_estimates(estimates_main)
-p
+
+plot_estimates(df)
+
+
+
 
