8e6ce3736e725f0116002def9b5076267dc60993
marwaalarab <73698404+marwaalarab@users.noreply.github.com>
2024-03-20 13:25:04 +0000

    merge last 2 stata outputs with data
    The stata output were done over 2 runs on the server hence, they are merged together here

--- analysis/post_release/make_plot_data.R
+++ analysis/post_release/make_plot_data.R
@@ -5,10 +5,10 @@ print('Load model output')
 output_dir <- "/Users/cu20932/Library/CloudStorage/OneDrive-SharedLibraries-UniversityofBristol/grp-EHR - OS outputs/death_fix20240305/"
 path_model_output<-paste0(output_dir,"model_output_midpoint6.csv")
 
+actvie_analyses<- readRDS("lib/active_analyses.rds")
 
 df <- readr::read_csv(path_model_output,
                       show_col_types = FALSE)
-
 df$source <- "R"
 
 # Restrict to plot data --------------------------------------------------------
@@ -25,7 +25,7 @@ tmp <- readr::read_csv(paste0(output_dir,"stata_model_output_midpoint6.csv"),
                        show_col_types = FALSE)
 tmp$outcome<- gsub("_cox_model","",tmp$outcome)
 tmp$name<- gsub("_cox_model","",tmp$name)
-tmp$name<-paste0("cohot_",tmp$name)
+tmp$name<-paste0("cohort_",tmp$name)
   
   tmp$source <- "Stata"
 
@@ -34,6 +34,21 @@ tmp$rank <- 1
 
 tmp <- tmp[,colnames(df)]
 df <- rbind(df,tmp)
+#Exceptionally merge 2 stata outputs together because we ran them on 2 batches 
+ 
+tmp2 <- readr::read_csv(paste0(output_dir,"OS output /stata_model_output_midpoint6.csv"),#second stata file 
+                         show_col_types = FALSE) 
+tmp2$outcome<- gsub("_cox_model","",tmp2$outcome)
+tmp2$name<- gsub("_cox_model","",tmp2$name)
+tmp2$name<-paste0("cohort_",tmp2$name)
+
+tmp2$source <- "Stata"
+
+df$rank <- 0
+tmp2$rank <- 1
+
+tmp2 <- tmp2[,colnames(df)]
+df <- rbind(df,tmp2)
 
 df <- df %>%
   dplyr::group_by(outcome,cohort,analysis) %>%
